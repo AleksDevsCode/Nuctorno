31fc22c1d5b3c0a4976fd30bc69816f68f335fa3
Plane Simulator lol <66514841+AleksDevsCode@users.noreply.github.com>
2023-05-09 21:09:55 +0100

    Update Header.tsx

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
 <nav className=" px-2 sm:px-4 py-2.5 bg-black bg-opacity-50">
   <div className="container flex flex-wrap items-center justify-between mx-auto">
     <a href="/" className="flex items-center">
-        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">APEX WEBSITES</span>
+         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">APEX BOTS</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
